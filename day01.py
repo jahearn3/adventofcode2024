9bc86884f5c4c840127837c6abbfd26dc1406bc7
jahearn3 <josephahearn3@gmail.com>
2024-11-30 21:12:30 -0800

    Day 1 complete

---
+++ day01.py
@@ -0,0 +1,39 @@
+# Day 1: Historian Hysteria
+
+import load_data as ld
+import os
+
+f = os.path.basename(__file__)
+day = f[3:5]
+
+# data = ld.load_data(f'example{day}.txt')
+data = ld.load_data(f'input{day}.txt')
+
+ans = 0
+
+a = []
+b = []
+
+for line in data:
+    a1, b1 = line.split('   ')
+    a.append(int(a1))
+    b.append(int(b1))
+
+a = sorted(a)
+b = sorted(b)
+
+for i in range(len(a)):
+    ans += abs(a[i] - b[i])
+
+print(ans)
+
+ans = 0
+
+for i in range(len(a)):
+    c = 0
+    for j in range(len(b)):
+        if a[i] == b[j]:
+            c += 1
+    ans += a[i] * c
+
+print(ans)
