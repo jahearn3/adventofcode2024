25add6f8ff0d3b383702307a2663556b006fc644
jahearn3 <josephahearn3@gmail.com>
2024-12-11 15:01:16 -0800

    Day 9 part 2 completed

--- day09.py
+++ day09.py
@@ -7,7 +7,7 @@ f = os.path.basename(__file__)
 day = f[3:5]
 
 data = ld.load_data(f'example{day}.txt')[0]
-# data = ld.load_data(f'input{day}.txt')[0]
+data = ld.load_data(f'input{day}.txt')[0]
 
 layout = []
 file_length = True
@@ -42,25 +42,37 @@ for i in range(len(compact_file)):
 print(ans)
 
 # Part 2
-compact_file = []
 idx_to_move = -1
-for i in range(len(layout) - free_space):
-    if layout[i] != '.':
-        compact_file.append(int(layout[i]))
+compact = False
+left_idx = next((i for i, v in enumerate(layout) if v == '.'), -1)
+right_idx = len(layout) - 1
+while left_idx < right_idx:
+    # Get next rightmost file
+    char = layout[right_idx]
+    if char != '.':
+        size = 0
+        right_idx2 = right_idx
+        while char == layout[right_idx2]:
+            size += 1
+            right_idx2 -= 1
+        slice_to_move_left = layout[right_idx2+1:right_idx+1]
+        # Look for leftmost free space
+        left_idx2 = left_idx
+        while left_idx2 < right_idx:
+            slice_to_move_right = layout[left_idx2:left_idx2+size]
+            if slice_to_move_right == ['.'] * size:
+                # Swap slices
+                layout = layout[:left_idx2] + slice_to_move_left + layout[left_idx2+size:right_idx2+1] + slice_to_move_right + layout[right_idx+1:]
+                left_idx = next((i for i, v in enumerate(layout) if v == '.'), -1)
+                break
+            else:
+                left_idx2 += 1
+        right_idx = right_idx2
     else:
-        # See how much free space in this slot
-        j = i
-        while layout[j] == '.':
-            j += 1
-        # Find next available files to move
-        # TODO- Here
-        while layout[idx_to_move] == '.':
-            idx_to_move -= 1
-
-        compact_file.append(int(layout[idx_to_move]))
-        idx_to_move -= 1
+        right_idx -= 1
 
 ans = 0
-for i in range(len(compact_file)):
-    ans += i * compact_file[i]
+for i in range(len(layout)):
+    if layout[i] != '.':
+        ans += i * int(layout[i])
 print(ans)
