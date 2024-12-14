58b0a122b578ec8e5345f7a898df3b0fd28579a7
jahearn3 <josephahearn3@gmail.com>
2024-12-14 14:43:01 -0800

    Day 14 Part 1 complete, Part 2 in progress

---
+++ day14.py
@@ -0,0 +1,102 @@
+# Day 14: Restroom Redoubt
+
+import load_data as ld
+import os
+
+f = os.path.basename(__file__)
+day = f[3:5]
+
+data = ld.load_data(f'example{day}.txt')
+data = ld.load_data(f'input{day}.txt')
+
+grid_x = 101
+grid_y = 103
+# grid_x = 11
+# grid_y = 7
+seconds = 100
+
+robots = []
+for line in data:
+    p, v = line.split(' ')
+    pos = p.split('=')[1]
+    vel = v.split('=')[1]
+    x, y = pos.split(',')
+    vx, vy = vel.split(',')
+    robots.append((int(x), int(y), int(vx), int(vy)))
+
+# print(robots)
+prev_robots = robots
+# prev_robots = [(2, 4, 2, -3)]
+# print(prev_robots)
+for t in range(seconds):
+    next_robots = []
+    for r in prev_robots:
+        x, y, vx, vy = r
+        x = (x + vx) % grid_x
+        y = (y + vy) % grid_y
+        next_robots.append((x, y, vx, vy))
+    # print(next_robots)
+    prev_robots = next_robots
+
+
+half_x = grid_x // 2
+half_y = grid_y // 2
+
+q1, q2, q3, q4 = 0, 0, 0, 0
+
+for r in next_robots:
+    x, y, vx, vy = r
+    # print(r)
+    if x < half_x and y < half_y:
+        q1 += 1
+    elif x > half_x and y < half_y:
+        q2 += 1
+    elif x < half_x and y > half_y:
+        q3 += 1
+    elif x > half_x and y > half_y:
+        q4 += 1
+print(q1, q2, q3, q4)
+ans = q1 * q2 * q3 * q4
+print(ans)
+
+
+# Part 2
+
+prev_robots = robots
+seconds = 10
+t = 0
+while True:
+    anomaly = False
+    t += 1
+    next_robots = []
+    for r in prev_robots:
+        x, y, vx, vy = r
+        x = (x + vx) % grid_x
+        y = (y + vy) % grid_y
+        next_robots.append((x, y, vx, vy))
+    if (t % 20) == 0:
+        print(f'{t} seconds have elapsed.')
+    # Already looked at first 400 seconds
+    # and 7000 < t < 7200
+    if t > 400:
+        for i in range(grid_y):
+            row = ''
+            for j in range(grid_x):
+                c = 0
+                for rob in next_robots:
+                    xx, yy, _, _ = rob
+                    if j == xx and i == yy:
+                        c += 1
+                if c == 0:
+                    row += '.'
+                else:
+                    row += '#'
+            if ('######') in row:
+                anomaly = True
+                print(f'After {t} seconds')
+            if anomaly:
+                print(row)
+
+    prev_robots = next_robots
+    if anomaly:
+        break
