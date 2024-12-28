d0f550d19a6f72224a8becca270e3b36484284ae
jahearn3 <josephahearn3@gmail.com>
2024-12-27 16:59:46 -0800

    Day 23 part 2 complete

--- day23.py
+++ day23.py
@@ -26,5 +26,34 @@ for x in graph:
             if x != z and x in graph[z]:
                 connected_triplets.add(tuple(sorted([x, y, z])))
 
-print(len([s for s in connected_triplets if any(cn.startswith('t') for cn in s)]))
+ans = 0
+for s in connected_triplets:
+    if any(cn.startswith('t') for cn in s):
+        ans += 1
+print(ans)
 
+# Part 2
+
+
+def bron_kerbosch(R, P, X):
+    '''
+    This recursive function finds all maximal cliques.
+    `R` is the current set of nodes in the clique,
+    `P` is the set of candidates for extending the clique, and
+    `X` is the set of excluded nodes.
+    When `P` and `X` are both empty, a maximal clique has been found.
+    '''
+    if not P and not X:
+        yield R  # Found a maximal clique
+    while P:
+        v = P.pop()
+        new_R = R | {v}
+        yield from bron_kerbosch(new_R, P & graph[v], X & graph[v])
+        X.add(v)
+
+
+maximal_cliques = list(bron_kerbosch(set(), set(graph.keys()), set()))
+largest_clique = max(maximal_cliques, key=len, default=set())
+
+largest_clique_sorted = sorted(largest_clique)
+print(','.join(largest_clique_sorted))
